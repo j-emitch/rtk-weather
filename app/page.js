e20f6fbf76d6c9bf9183ede17110ec4d65ee4c3a
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-03-26 15:18:00 -0400

    fix: add duplicate cities and formatting

--- app/page.js
+++ app/page.js
@@ -1,45 +1,53 @@
-'use client';
-import { useState } from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { addCity } from './store/slices/cities';
-import CityWeather from './components/CityWeather';
-import 'bootstrap/dist/css/bootstrap.min.css';
+"use client";
+import { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { addCity } from "./store/slices/cities";
+import CityWeather from "./components/CityWeather";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Home() {
   const cities = useSelector((state) => state.cities.cities);
   const dispatch = useDispatch();
-  const [newCity, setNewCity] = useState('');
+  const [newCity, setNewCity] = useState("");
 
-  const handleAddCity = () => {
-    if (newCity) {
-      dispatch(addCity(newCity));
-      setNewCity('');
-    }
-  };
+const handleAddCity = () => {
+  if (!cities.some((city) => city.name.toLowerCase() === newCity.toLowerCase())) {
+    dispatch(addCity(newCity));
+  }
+  setNewCity("");
+};
 
   return (
-    <div className='container mt-5'>
-      <h1 className='text-center mb-4'>Weather Forecast</h1>
-      <div className='input-group mb-3'>
+    <div className="container mt-5">
+      <h1 className="text-center mb-4">Weather Forecast</h1>
+      <div className="input-group mb-3">
         <input
-          type='text'
+          type="text"
           value={newCity}
           onChange={(e) => setNewCity(e.target.value)}
-          placeholder='Add a city'
-          className='form-control'
+          placeholder="Add a city"
+          className="form-control"
         />
-        <button onClick={handleAddCity} className='btn btn-primary'>
+        <button onClick={handleAddCity} className="btn btn-primary">
           Submit
         </button>
       </div>
       <div>
-        <table className='table'>
+        <table className="table">
           <thead>
             <tr>
-              <th scope='col'>City</th>
-              <th scope='col'>Temp</th>
-              <th scope='col'>Pressure</th>
-              <th scope='col'>Humidity</th>
+              <th className="text-center col-1" scope="col">
+                City
+              </th>
+              <th className="text-center col-3" scope="col">
+                Temp
+              </th>
+              <th className="text-center col-3" scope="col">
+                Pressure
+              </th>
+              <th className="text-center col-3" scope="col">
+                Humidity
+              </th>
             </tr>
           </thead>
           <tbody>
