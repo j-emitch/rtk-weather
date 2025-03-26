1efa0dfc8dfce72da3a6a7ccab04c0603153bec2
Joseph Mitchell <joseph.e.mitchell17@gmail.com>
2025-03-26 15:18:40 -0400

    add formatting

--- app/components/CityWeather.js
+++ app/components/CityWeather.js
@@ -3,32 +3,32 @@ import "bootstrap/dist/css/bootstrap.min.css";
 
 const CityWeather = ({ city }) => {
   return (
-      <tr>
-        <td>
-          <h5>{city.name}</h5>
-        </td>
-        <td>
-          <Sparklines data={city.temp}>
-            <SparklinesLine color="orange" />
-            <SparklinesReferenceLine type="mean" />
-          </Sparklines>
-          <p>{city.avgTemp} &deg;F</p>
-        </td>
-        <td>
-          <Sparklines data={city.pressure}>
-            <SparklinesLine color="green" />
-            <SparklinesReferenceLine type="mean" />
-          </Sparklines>
-          <p>{city.avgPressure} hPa</p>
-        </td>
-        <td>
-          <Sparklines data={city.humidity}>
-            <SparklinesLine color="gray" />
-            <SparklinesReferenceLine type="mean" />
-          </Sparklines>
-          <p>{city.avgHumidity} %</p>
-        </td>
-      </tr>
+    <tr>
+      <td>
+        <h5 className="align-middle text-center">{city.name}</h5>
+      </td>
+      <td >
+        <Sparklines data={city.temp}>
+          <SparklinesLine color="orange" />
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
+        <p className="text-center">{city.avgTemp} &deg;F</p>
+      </td>
+      <td>
+        <Sparklines data={city.pressure}>
+          <SparklinesLine color="green" />
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
+        <p className="text-center">{city.avgPressure} hPa</p>
+      </td>
+      <td>
+        <Sparklines data={city.humidity}>
+          <SparklinesLine color="gray" />
+          <SparklinesReferenceLine type="mean" />
+        </Sparklines>
+        <p className="text-center">{city.avgHumidity} %</p>
+      </td>
+    </tr>
   );
 };
 
